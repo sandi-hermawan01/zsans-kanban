bc35effd284b58c01349582d4aae45c03f7cb759
sandi-hermawan01 <sandihermawanf01@gmail.com>
2024-01-23 19:39:53 +0700

    add tailwind postcss autoprefixer

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -1,161 +1,180 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { Box, Drawer, IconButton, List, ListItem, ListItemButton, Typography } from '@mui/material'
-import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined'
-import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined'
-import { Link, useNavigate, useParams } from 'react-router-dom'
-import assets from '../../assets/index'
-import { useEffect, useState } from 'react'
-import boardApi from '../../api/boardApi'
-import { setBoards } from '../../redux/features/boardSlice'
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
-import FavouriteList from './FavouriteList'
+import { useSelector, useDispatch } from "react-redux";
+import {
+  Box,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  Typography,
+} from "@mui/material";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import assets from "../../assets/index";
+import { useEffect, useState } from "react";
+import boardApi from "../../api/boardApi";
+import { setBoards } from "../../redux/features/boardSlice";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import FavouriteList from "./FavouriteList";
 
 const Sidebar = () => {
-  const user = useSelector((state) => state.user.value)
-  const boards = useSelector((state) => state.board.value)
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const { boardId } = useParams()
-  const [activeIndex, setActiveIndex] = useState(0)
+  const user = useSelector((state) => state.user.value);
+  const boards = useSelector((state) => state.board.value);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { boardId } = useParams();
+  const [activeIndex, setActiveIndex] = useState(0);
 
-  const sidebarWidth = 250
+  const sidebarWidth = 250;
 
   useEffect(() => {
     const getBoards = async () => {
       try {
-        const res = await boardApi.getAll()
-        dispatch(setBoards(res))
+        const res = await boardApi.getAll();
+        dispatch(setBoards(res));
       } catch (err) {
-        alert(err)
+        alert(err);
       }
-    }
-    getBoards()
-  }, [dispatch])
+    };
+    getBoards();
+  }, [dispatch]);
 
   useEffect(() => {
-    const activeItem = boards.findIndex(e => e.id === boardId)
+    const activeItem = boards.findIndex((e) => e.id === boardId);
     if (boards.length > 0 && boardId === undefined) {
-      navigate(`/boards/${boards[0].id}`)
+      navigate(`/boards/${boards[0].id}`);
     }
-    setActiveIndex(activeItem)
-  }, [boards, boardId, navigate])
+    setActiveIndex(activeItem);
+  }, [boards, boardId, navigate]);
 
   const logout = () => {
-    localStorage.removeItem('token')
-    navigate('/login')
-  }
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
 
   const onDragEnd = async ({ source, destination }) => {
-    const newList = [...boards]
-    const [removed] = newList.splice(source.index, 1)
-    newList.splice(destination.index, 0, removed)
+    const newList = [...boards];
+    const [removed] = newList.splice(source.index, 1);
+    newList.splice(destination.index, 0, removed);
 
-    const activeItem = newList.findIndex(e => e.id === boardId)
-    setActiveIndex(activeItem)
-    dispatch(setBoards(newList))
+    const activeItem = newList.findIndex((e) => e.id === boardId);
+    setActiveIndex(activeItem);
+    dispatch(setBoards(newList));
 
     try {
-      await boardApi.updatePositoin({ boards: newList })
+      await boardApi.updatePositoin({ boards: newList });
     } catch (err) {
-      alert(err)
+      alert(err);
     }
-  }
+  };
 
   const addBoard = async () => {
     try {
-      const res = await boardApi.create()
-      const newList = [res, ...boards]
-      dispatch(setBoards(newList))
-      navigate(`/boards/${res.id}`)
+      const res = await boardApi.create();
+      const newList = [res, ...boards];
+      dispatch(setBoards(newList));
+      navigate(`/boards/${res.id}`);
     } catch (err) {
-      alert(err)
+      alert(err);
     }
-  }
+  };
 
   return (
     <Drawer
       container={window.document.body}
-      variant='permanent'
+      variant="permanent"
       open={true}
       sx={{
         width: sidebarWidth,
-        height: '100vh',
-        '& > div': { borderRight: 'none' }
+        height: "100vh",
+        "& > div": { borderRight: "none" },
       }}
     >
       <List
         disablePadding
         sx={{
           width: sidebarWidth,
-          height: '100vh',
-          backgroundColor: assets.colors.secondary
+          height: "100vh",
+          backgroundColor: assets.colors.secondary,
         }}
       >
         <ListItem>
-          <Box sx={{
-            width: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between'
-          }}>
-            <Typography variant='body2' fontWeight='700'>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
               {user.username}
             </Typography>
             <IconButton onClick={logout}>
-              <LogoutOutlinedIcon fontSize='small' />
+              <LogoutOutlinedIcon fontSize="small" />
             </IconButton>
           </Box>
         </ListItem>
-        <Box sx={{ paddingTop: '10px' }} />
+        <Box sx={{ paddingTop: "10px" }} />
         <FavouriteList />
-        <Box sx={{ paddingTop: '10px' }} />
+        <Box sx={{ paddingTop: "10px" }} />
         <ListItem>
-          <Box sx={{
-            width: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between'
-          }}>
-            <Typography variant='body2' fontWeight='700'>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
               Private
             </Typography>
             <IconButton onClick={addBoard}>
-              <AddBoxOutlinedIcon fontSize='small' />
+              <AddBoxOutlinedIcon fontSize="small" />
             </IconButton>
           </Box>
         </ListItem>
         <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable key={'list-board-droppable-key'} droppableId={'list-board-droppable'}>
+          <Droppable
+            key={"list-board-droppable-key"}
+            droppableId={"list-board-droppable"}
+          >
             {(provided) => (
               <div ref={provided.innerRef} {...provided.droppableProps}>
-                {
-                  boards.map((item, index) => (
-                    <Draggable key={item.id} draggableId={item.id} index={index}>
-                      {(provided, snapshot) => (
-                        <ListItemButton
-                          ref={provided.innerRef}
-                          {...provided.dragHandleProps}
-                          {...provided.draggableProps}
-                          selected={index === activeIndex}
-                          component={Link}
-                          to={`/boards/${item.id}`}
+                {boards.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <ListItemButton
+                        ref={provided.innerRef}
+                        {...provided.dragHandleProps}
+                        {...provided.draggableProps}
+                        selected={index === activeIndex}
+                        component={Link}
+                        to={`/boards/${item.id}`}
+                        sx={{
+                          pl: "20px",
+                          cursor: snapshot.isDragging
+                            ? "grab"
+                            : "pointer!important",
+                        }}
+                      >
+                        <Typography
+                          variant="body2"
+                          fontWeight="700"
                           sx={{
-                            pl: '20px',
-                            cursor: snapshot.isDragging ? 'grab' : 'pointer!important'
+                            whiteSpace: "nowrap",
+                            overflow: "hidden",
+                            textOverflow: "ellipsis",
                           }}
                         >
-                          <Typography
-                            variant='body2'
-                            fontWeight='700'
-                            sx={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
-                          >
-                            {item.icon} {item.title}
-                          </Typography>
-                        </ListItemButton>
-                      )}
-                    </Draggable>
-                  ))
-                }
+                          {item.icon} {item.title}
+                        </Typography>
+                      </ListItemButton>
+                    )}
+                  </Draggable>
+                ))}
                 {provided.placeholder}
               </div>
             )}
@@ -163,7 +182,7 @@ const Sidebar = () => {
         </DragDropContext>
       </List>
     </Drawer>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { store } from './redux/store'
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-
     <App />
   </Provider>
 );
